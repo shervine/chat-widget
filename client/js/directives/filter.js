4122bf709608534d276085b5b4562d3194324b5c
Leonard Labuneti <leonard.labuneti@gmail.com>
2017-11-13 09:28:01 -0800

    Switched frontend to angularjs, some basic directives

---
+++ client/js/directives/filter.js
@@ -0,0 +1,15 @@
+// setTimeout(function () {
+//   (function () {
+//     angular.module('menChat')
+//      .directive('filter', function () {
+//       return {
+//         restrict: 'E',
+//         scope: {},
+//         link: function () {
+//           console.log('directive filter link function ');
+//         },
+//         templateUrl: 'filter.html'
+//       }
+//     });
+//   })();
+// }, 500);
--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,58 @@
 if (Meteor.isClient) {
   users = new PgSubscription('allUsers');
-  Template.usersTpl.helpers({
-    bootcamps() {
-      return Bootcamps.select();
-    },
-    users() {
-      return users.reactive();
-    }
-  });
-  Template.usersTpl.events({
-    "click .user-row"(ev, a) {
-      console.log('Clicked user ', ev.target.attributes["user-id"]);
-      var selUserId = ev.target.attributes["user-id"];
-      //Meteor.call("/selectedUser/" + selUserId, (err) => { err && alert(err) });
-    }
-  });
-} 
+  // Template.usersTpl.helpers({
+  //   bootcamps() {
+  //     return Bootcamps.select();
+  //   },
+  //   users() {
+  //     return users.reactive();
+  //   }
+  // });
+  // Template.usersTpl.events({
+  //   "click .user-row" (ev, a) {
+  //     console.log('Clicked user ', ev.target.attributes["user-id"]);
+  //     var selUserId = ev.target.attributes["user-id"];
+  //     //Meteor.call("/selectedUser/" + selUserId, (err) => { err && alert(err) });
+  //   }
+  // });
+
+
+  // setTimeout(function () {
+
+  // }, 300);
+
+}
+
+(function () {
+  angular.module('menChat', ['ui.bootstrap'])
+    .config(function () {
+      console.log('angular menchat config triggered');
+    })
+    .run(function ($rootScope) {
+      console.log('angular menchat run triggered');
+      $rootScope.users = users.reactive();
+    })
+    .directive('filter', function () {
+      return {
+        restrict: 'E',
+        scope: {},
+        link: function ($scope) {
+          console.log('directive filter link function ');
+          $scope.checkModel = {'left': false , 'right': false};
+
+        },
+        templateUrl: 'filter.html'
+      }
+    })
+    .directive('usersList', function ($rootScope) {
+      return {
+        restrict: 'E',
+        scope: {},
+        link: function ($scope, iElem, iAttr) {
+          console.log('directive usersList link function ');
+          $scope.users = $rootScope.users;
+        },
+        templateUrl: 'users-list.html'
+      }
+    })
+})();
