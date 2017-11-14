2e3894857bf03f2a85fa78388ed845601ca17c12
Leonard Labuneti <leonard.labuneti@gmail.com>
2017-11-13 19:53:14 -0800

    Select User - Show Messages

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,6 @@
 users = new PgSubscription('allUsers');
 
+
 (function () {
   angular.module('menChat', ['ui.bootstrap'])
     .config(function () {
@@ -29,10 +30,45 @@ users = new PgSubscription('allUsers');
           console.log('directive usersList link function ');
           $scope.users = users.reactive();
           $timeout(function(){
-            angular.element('.users-list').css('max-height', '600px');
+            angular.element('.users-list').css('max-height', '700px');
           },300);
+
+          $scope.userSelected = function(user){
+            console.log('Selected user ',user);
+            
+            // Meteor.call('userMess', user.u_id);
+            var messages = new PgSubscription('userMessages', user.u_id);
+
+           $timeout(function(){
+             if(!messages.ready()){
+               console.log('Subscription not ready');
+             }
+            $scope.messages = messages.reactive();
+            console.log('Got messages ', $scope.messages);
+            $rootScope.$broadcast('selected_user_messages', $scope.messages);
+           },300);
+           
+          }
         },
         templateUrl: 'users-list.html'
+      }
+    })
+    .directive('userMessages', function ($rootScope, $timeout) {
+      return {
+        restrict: 'E',
+        scope: {},
+        link: function ($scope, iElem, iAttr) {
+          $scope.msgs =[];
+          $scope.$on('selected_user_messages', function(ev, messages){
+            $scope.msgs = messages;
+            console.log('Got messages ', $scope.msgs);
+          });
+
+          $timeout(function(){
+
+          });
+        },
+        templateUrl: 'user-messages.html'
       }
     })
 })();
--- server/tables.js
+++ server/tables.js
@@ -1,11 +1,24 @@
 users = new PgSubscription('allUsers');
+messages = new PgSubscription('userMessages');
 
-if (Meteor.isServer) {
-  liveDb = new LivePg(process.env.POSTGRESQL_URL, process.env.CHANNEL);
-  Meteor.publish('allUsers', function () {
-    var res = liveDb.select('SELECT v5_users.*, b.*  FROM v5_users \
+liveDb = new LivePg(process.env.POSTGRESQL_URL, process.env.CHANNEL);
+Meteor.publish('allUsers', function () {
+  var res = liveDb.select('SELECT v5_users.*, b.*  FROM v5_users \
                             LEFT JOIN v5_bootcamps b ON b.b_creator_id = v5_users.u_id\
-                            ORDER BY v5_users.u_id DESC limit 10');
-    return res;
-  });
-} 
+                            ORDER BY v5_users.u_id DESC limit 1000');
+  return res;
+});
+Meteor.publish('userMessages', function (userId) {
+  if (!userId) {
+    return [];
+  }
+
+  console.log('userMessages passed user id ', userId)
+
+  var res = liveDb.select('select e_message, e_creator_id, e_type_id  from v5_engagements \
+                          left join v5_engagement_types on a_id = e_type_id \
+                          where e_type_id in (6,7) and e_creator_id = '+userId+' \
+                          order by e_timestamp desc \
+                          limit 100');
+  return res;
+});
