bcb85977880e9c76e3f2bd6d193da7bf1e2e563b
Leonard Labuneti <leonard.labuneti@gmail.com>
2017-11-17 14:00:00 -0800

    css fixes

--- client/js/index.js
+++ client/js/index.js
@@ -3,45 +3,49 @@ messages = new PgSubscription('userMessages');
 
 (function () {
   angular.module('menChat', ['ui.bootstrap'])
-    .run(function($location, $rootScope){
+    .run(function ($location, $rootScope) {
       users = new PgSubscription('allUsers');
       $rootScope.instructorId = 82;
       $rootScope.bootcampId = 1;
       var bId = $location.search().bootcampId;
       var instrId = $location.search().instructorId;
-      if(bId){
-        $rootScope.bootcampId = bId; 
+      if (bId) {
+        $rootScope.bootcampId = bId;
       }
-      if(instrId){
-        $rootScope.instructorId = instrId; 
+      if (instrId) {
+        $rootScope.instructorId = instrId;
       }
       console.log('Using bootcamp id ', $rootScope.bootcampId);
       console.log('Using instructor id ', $rootScope.instructorId);
 
-      bootcampClasses = new PgSubscription('bootcampClasses', $rootScope.bootcampId, $rootScope.instructorId);   
+      bootcampClasses = new PgSubscription('bootcampClasses', $rootScope.bootcampId, $rootScope.instructorId);
     })
     .directive('filter', function ($timeout, $rootScope) {
       return {
         restrict: 'E',
         scope: {},
         link: function ($scope) {
-          $scope.class = {'r_start_date': 'All'};
+          $scope.class = {
+            'r_start_date': 'All'
+          };
           $scope.statuses = ['All', 'Inactive', 'Pending', 'Active'];
           $scope.status = $scope.statuses[0];
-          $scope.classes = bootcampClasses.reactive(); 
-        
-          $scope.selectClass = function (classObj){
+          $scope.classes = bootcampClasses.reactive();
+
+          $scope.selectClass = function (classObj) {
             console.log('Selected class ', classObj);
-            $scope.class = typeof classObj !== 'undefined' ? classObj : {'r_start_date': 'All'} ;
+            $scope.class = typeof classObj !== 'undefined' ? classObj : {
+              'r_start_date': 'All'
+            };
           }
-          $scope.selectStatus = function (statusObj){
+          $scope.selectStatus = function (statusObj) {
             $scope.status = statusObj;
           }
-          $scope.$watch('class', function(newVal){
-              if(!newVal){
-                return;
-              }
-              $rootScope.$broadcast('filter-class', newVal);
+          $scope.$watch('class', function (newVal) {
+            if (!newVal) {
+              return;
+            }
+            $rootScope.$broadcast('filter-class', newVal);
           });
         },
         templateUrl: 'filter.html'
@@ -54,7 +58,7 @@ messages = new PgSubscription('userMessages');
         link: function ($scope, iElem, iAttr) {
           $scope.searchTerm = '';
           $scope.allUsers = users.reactive();
-          
+
           $timeout(function () {
             // angular.element('.users-list').css('max-height', '700px');
             $scope.users = $scope.allUsers;
@@ -85,20 +89,20 @@ messages = new PgSubscription('userMessages');
 
           }
 
-          $scope.$on('filter-class', function(ev, classObj){
-            if(typeof classObj.r_id !== 'undefined') {
+          $scope.$on('filter-class', function (ev, classObj) {
+            if (typeof classObj.r_id !== 'undefined') {
               users = new PgSubscription('allUsers', classObj.r_id);
             } else {
               //no filter
               users = new PgSubscription('allUsers');
             }
             $scope.allUsers = users.reactive();
-            $timeout(function(){
+            $timeout(function () {
               $scope.users = $scope.allUsers;
               console.log('Filtered users ', $scope.users);
             }, 300);
-            
-            
+
+
           });
         },
         templateUrl: 'users-list.html'
@@ -107,67 +111,74 @@ messages = new PgSubscription('userMessages');
     .directive('userMessages', function ($rootScope, $timeout) {
       return {
         restrict: 'E',
-        scope: {selectedUser: '='},
+        scope: {
+          selectedUser: '='
+        },
         link: function ($scope, iElem, iAttr) {
           $scope.msgs = [];
-          $scope.$watch('selectedUser', function(newVal){
-               if(!newVal) {
-                  return;
-               }
+          $scope.$watch('selectedUser', function (newVal) {
+            if (!newVal) {
+              return;
+            }
+            $scope.loading = true;
+            messages = new PgSubscription('userMessages', $scope.selectedUser.u_id);
 
-               messages = new PgSubscription('userMessages', $scope.selectedUser.u_id);
-               
-               $timeout(function(){
-                $scope.msgs = messages.reactive().reverse();
-                console.log('User messages :', $scope.msgs); 
-                        $timeout(function () {
-                          var d = angular.element('.user-messages');
-                          d.animate({ scrollTop: d.prop('scrollHeight') }, 1);
-                        }, 300);
-               }, 300);
+            $timeout(function () {
+              $scope.msgs = messages.reactive().reverse();
+              console.log('User messages :', $scope.msgs);
+              $timeout(function () {
+                var d = angular.element('.user-messages');
+                d.animate({
+                  scrollTop: d.prop('scrollHeight')
+                }, 1);
+              }, 300);
+              $scope.loading = false;
+            }, 300);
           });
         },
         templateUrl: 'user-messages.html'
       }
     })
-    .directive('userDetails', function($rootScope, $timeout){
+    .directive('userDetails', function ($rootScope, $timeout) {
       return {
         restrict: 'E',
-        scope: {selectedUser: '='},
+        scope: {
+          selectedUser: '='
+        },
         link: function ($scope, iElem, iAttr) {
           $scope.userDetails1 = {};
-          $scope.$watch('selectedUser', function(newVal){
-              if(!newVal) {
-                  return;
-               }
- 
+          $scope.$watch('selectedUser', function (newVal) {
+            if (!newVal) {
+              return;
+            }
+
             userData = new PgSubscription('userData', $scope.selectedUser.u_id);
             $scope.details = userData.reactive();
-            $timeout(function(){
-              $scope.userDetails1 =  typeof $scope.details[0] !== 'undefined' ? $scope.details[0] : {} ; 
+            $timeout(function () {
+              $scope.userDetails1 = typeof $scope.details[0] !== 'undefined' ? $scope.details[0] : {};
               console.log('User details : ', $scope.userDetails1);
             }, 300);
-           
+
           });
         },
         templateUrl: 'user-details.html'
       }
     })
-    .controller('chatCtrl', function($scope, $rootScope){
+    .controller('chatCtrl', function ($scope, $rootScope) {
 
       $scope.chatInput = '';
-      $scope.sendMessage = function (){
+      $scope.sendMessage = function () {
         // var auth_hash = md5( sender_u_id . receiver_u_id . message_type . '7H6hgtgtfii87' )
-        if($scope.chatInput === ''){
+        if ($scope.chatInput === '') {
           alert('Please type the message you want to send');
           return;
         }
         var postObj = {
           'message': $scope.chatInput
         }
-        Meteor.call('sendChatMessage', postObj, function(err,success){
-           console.log('sendChatMessage ',err,success);
-           $scope.chatInput = '';
+        Meteor.call('sendChatMessage', postObj, function (err, success) {
+          console.log('sendChatMessage ', err, success);
+          $scope.chatInput = '';
         });
       }
     })
