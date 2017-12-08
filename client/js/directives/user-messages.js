8f90d662906b256ab20508845d1c3535388ff2f4
Guest2 <guest2@vacsal52958.local>
2017-12-07 21:12:39 -0800

    UI Tweaks @ Center Column show images and videos, closes #30

--- client/js/directives/user-messages.js
+++ client/js/directives/user-messages.js
@@ -43,6 +43,15 @@ angular.module('menChat')
           $scope.messages.stop();
         }
 
+        $rootScope.$watch('instructorData', function(newData){
+            if (!newData) {
+              return;
+            }
+
+            $scope.instructorData = newData[0] ? newData[0] : null;
+            console.log('Instructor data ', $scope.instructorData);
+        });
+
         $scope.messages = new PgSubscription('userMessages', $scope.selectedUser.u_id).reactive();
 
         $scope.messages.addEventListener('updated', function(diff, data){
@@ -89,5 +98,76 @@ angular.module('menChat')
       return null;
     }
     return items.slice().reverse();
+  };
+})
+.filter('formatMsg', function($sce) {
+  return function($e_message) {
+    
+        // console.log('Formatting msg ', $e_message);
+
+        function linkify(inputText) {
+            var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+            //URLs starting with http://, https://, or ftp://
+            replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+            replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+            //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+            replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+            replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+            //Change email addresses to mailto:: links.
+            replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+            replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+            return replacedText;
+        }
+
+         function nl2br (str, is_xhtml) {
+            // var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+            // return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+
+            return str.replace('<br />', "<br>");
+         }
+        
+                if($e_message.indexOf('/attach') >= 0 ){
+
+                  var $attachementsTxt = $e_message.split(' ');
+                  console.log('aaa ', $attachementsTxt);
+                  if(typeof $attachementsTxt[1] !== 'undefined'){
+                    var attachement = $attachementsTxt[1];
+                    // attachements = attachements.split(':');
+                  } 
+                  if(!attachement) {
+                    return;
+                  }
+                  console.log('Attachement: ', attachement);
+
+                      var $segments = attachement.split(':https:');
+                      var $sub_segments = 'https:' + $segments[1]; 
+
+                      console.log('Attachement type : ', $segments[0]);
+                      if($segments[0]=='image'){
+                          $e_message = '<a href="'+$sub_segments+'" target="_blank"><img src="'+
+                          $sub_segments+'" style="max-width:100%" /></a>';
+                      } else if($segments[0]=='audio'){
+                          $e_message = '<audio controls><source src="'+$sub_segments+'" type="audio/mpeg"></audio>';
+                      } else if($segments[0]=='video'){
+                          $e_message = '<video width="100%" onclick="this.play()" controls><source src="'+
+                          $sub_segments+'" type="video/mp4"></video>';
+                      } else if($segments[0]=='file'){
+                          $e_message = '<a href="'+$sub_segments+'" class="btn btn-primary" target="_blank">'+
+                          '<i class="fa fa-cloud-download" aria-hidden="true"></i> Download File</a>';
+                      }
+                      
+              } else {
+                  $e_message = linkify($e_message);
+              }
+
+              $e_message = nl2br($e_message);
+              
+
+              return $sce.trustAsHtml($e_message);
+        
   };
 });
--- client/lib/index.js
+++ client/lib/index.js
@@ -1,18 +1,24 @@
 
-angular.module('menChat', ['ui.bootstrap', 'ui.bootstrap.tpls'])
+angular.module('menChat', ['ui.bootstrap', 'ui.bootstrap.tpls', 'ui.bootstrap.tooltip'])
     .run(function ($location, $rootScope, $interval) {
+      
+      //default bootstrap vars
       $rootScope.instructorId = 1;
       $rootScope.bootcampId = 1;
+
       var bId = $location.search().bootcampId;
       var instrId = $location.search().instructorId;
+
       if (bId) {
         $rootScope.bootcampId = bId;
       }
       if (instrId) {
         $rootScope.instructorId = instrId;
       }
+
       console.log('Using bootcamp id ', $rootScope.bootcampId);
       console.log('Using instructor id ', $rootScope.instructorId);
 
       $rootScope.bootcampClasses = new PgSubscription('bootcampClasses', $rootScope.bootcampId, $rootScope.instructorId);
+      $rootScope.instructorData  = new PgSubscription('instructorData', $rootScope.instructorId).reactive();
     });
