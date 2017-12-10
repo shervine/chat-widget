aeeae0e5bda631f3ef4b007808125f996d569c3a
Leonard <leonard.labuneti@gmail.com>
2017-12-10 07:38:35 -0800

    #30 css color fix, last messages render if attachment

--- client/js/directives/users-list.js
+++ client/js/directives/users-list.js
@@ -1,74 +1,91 @@
 angular.module('menChat')
-.directive('usersList', function ($rootScope, $timeout, $interval) {
-  return {
-    restrict: 'E',
-    scope: {},
-    link: function ($scope, iElem, iAttr) {
-      $scope.searchTerm = '';
+  .directive('usersList', function ($rootScope, $timeout, $interval) {
+    return {
+      restrict: 'E',
+      scope: {},
+      link: function ($scope, iElem, iAttr) {
+        $scope.searchTerm = '';
 
-      if ($scope.allUsers && typeof $scope.allUsers.stop == 'function') {
-        $scope.allUsers.stop();
-      }
-
-    $scope.allUsers = new PgSubscription('allUsers').reactive();
-
-     var stop;
-     $scope.loading = true;
-     stop = $interval(function(){
-        if(!$scope.allUsers.ready()){
-          return;
+        if ($scope.allUsers && typeof $scope.allUsers.stop == 'function') {
+          $scope.allUsers.stop();
         }
-        $scope.users = $scope.allUsers;
-        console.log('allUsers populated ', $scope.users);
-        $scope.stopInterval();
-      }, 60);
 
-      $scope.stopInterval = function() {
-       if (angular.isDefined(stop)) {
-            $scope.loading = false;
-           $interval.cancel(stop);
-           stop = undefined;
-         }
-      };
+        $scope.allUsers = new PgSubscription('allUsers').reactive();
 
-      $scope.userSelected = function (user) {
-        $rootScope.selectedUser = user;
-        $scope.selectedUser = user;
-      }
-
-      $scope.search = function (searchTerm) {
-        if (searchTerm === '' || searchTerm === ' ') {
+        var stop;
+        $scope.loading = true;
+        stop = $interval(function () {
+          if (!$scope.allUsers.ready()) {
+            return;
+          }
           $scope.users = $scope.allUsers;
-          return;
+          console.log('allUsers populated ', $scope.users);
+          $scope.stopInterval();
+        }, 60);
+
+        $scope.stopInterval = function () {
+          if (angular.isDefined(stop)) {
+            $scope.loading = false;
+            $interval.cancel(stop);
+            stop = undefined;
+          }
+        };
+
+        $scope.userSelected = function (user) {
+          $rootScope.selectedUser = user;
+          $scope.selectedUser = user;
         }
 
-        $scope.users = [];
-        searchTerm = searchTerm.toLowerCase();
-        for (var i in $scope.allUsers) {
-          var fname = $scope.allUsers[i].u_fname.toLowerCase();
-          var lname = $scope.allUsers[i].u_lname.toLowerCase();
-          if (fname.indexOf(searchTerm) >= 0 || lname.indexOf(searchTerm) >= 0) {
-            $scope.users.push($scope.allUsers[i]);
+        $scope.search = function (searchTerm) {
+          if (searchTerm === '' || searchTerm === ' ') {
+            $scope.users = $scope.allUsers;
+            return;
+          }
+
+          $scope.users = [];
+          searchTerm = searchTerm.toLowerCase();
+          for (var i in $scope.allUsers) {
+            var fname = $scope.allUsers[i].u_fname.toLowerCase();
+            var lname = $scope.allUsers[i].u_lname.toLowerCase();
+            if (fname.indexOf(searchTerm) >= 0 || lname.indexOf(searchTerm) >= 0) {
+              $scope.users.push($scope.allUsers[i]);
+            }
           }
         }
-      }
 
-      $scope.$on('new-filter', function (ev, filterObj) {
-        $scope.loading = true;
-        $scope.allUsers = new PgSubscription('allUsers', filterObj).reactive();
-        $rootScope.selectedUser = null;
-        $scope.selectedUser = null;
+        $scope.renderLastMessage = function (message) {
+          if (message.indexOf('image:') >= 0 && message.indexOf('/attach') >= 0) {
+            return 'image';
+          }
+          if (message.indexOf('audio:') >= 0 && message.indexOf('/attach') >= 0) {
+            return 'audio';
+          }
+          if (message.indexOf('video:') >= 0 && message.indexOf('/attach') >= 0) {
+            return 'video';
+          }
+          if (message.indexOf('file:') >= 0 && message.indexOf('/attach') >= 0) {
+            return 'file';
+          }
 
-        stop = $interval(function(){
-           if(!$scope.allUsers.ready()){
-             return;
-           }
-           $scope.users = $scope.allUsers;
-           console.log('allUsers populated ', $scope.users);
-           $scope.stopInterval();
-         }, 60);
-      });
-    },
-    templateUrl: 'users-list.html'
-  }
-})
+          return message;
+        }
+
+        $scope.$on('new-filter', function (ev, filterObj) {
+          $scope.loading = true;
+          $scope.allUsers = new PgSubscription('allUsers', filterObj).reactive();
+          $rootScope.selectedUser = null;
+          $scope.selectedUser = null;
+
+          stop = $interval(function () {
+            if (!$scope.allUsers.ready()) {
+              return;
+            }
+            $scope.users = $scope.allUsers;
+            console.log('allUsers populated ', $scope.users);
+            $scope.stopInterval();
+          }, 60);
+        });
+      },
+      templateUrl: 'users-list.html'
+    }
+  })
