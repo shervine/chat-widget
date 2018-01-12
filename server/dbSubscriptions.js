0195079069b78da6f1458c4de3280e4f8532fb12
Leonard <leonard.labuneti@gmail.com>
2018-01-11 17:41:28 -0800

    #53 Adjusted non-chat engagements messages

--- server/dbSubscriptions.js
+++ server/dbSubscriptions.js
@@ -80,7 +80,7 @@ Meteor.publish('allUsers', function (filterObj, authObj) {
   
     var mSubscription = liveDb.select('select e.*, a.a_name from v5_engagements e \
                             left join v5_engagement_types a on a_id = e_type_id \
-                            where e_type_id in (6,7,9,12,26,27,28,29,30,31,32,33,38,41,43,44) and (e_initiator_u_id = $1 or \
+                            where e_type_id in (6,7,9,12,26,27,29,30,31,32,33,38,41,43,44) and (e_initiator_u_id = $1 or \
                             e_recipient_u_id = $1)\
                             order by e_timestamp DESC \
                             limit 100', [userId]);
