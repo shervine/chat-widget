3a5107f9e858d0d534fd0dafdaf478fd4ee48eb1
Leonard Labuneti <leonard.labuneti@gmail.com>
2018-02-09 08:39:39 -0800

    #87 Amazon S3 upload mime type

--- server/mench-aws.js
+++ server/mench-aws.js
@@ -1,7 +1,6 @@
 // var exports = module.exports = {};
 
 var AWS = Npm.require('aws-sdk');
-// var     fs = Npm.require('fs');
 
 AWS.config.update({ accessKeyId: 'AKIAJOLBLKFSYCCYYDRA', secretAccessKey: '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' });
 
@@ -10,30 +9,28 @@ var s3 = new AWS.S3();
 menchUpload = function(fileBlob, fileName, cb) {
 
   var base64data = new Buffer(fileBlob, 'binary');
+  var metaData = getContentTypeByFile(fileName);
+  var ext = fileName.split('.')[1];
 
     s3.putObject({
       Bucket: 'mench-chat',
       Key: fileName,
       Body: base64data,
-      ACL: 'public-read'
+      ACL: 'public-read',
+      ContentType: metaData
     },cb);
 };
 
-// Read in the file, convert it to base64, store to S3
-// fs.readFile('del.txt', function (err, data) {
-//   if (err) { throw err; }
-//
-//   var base64data = new Buffer(data, 'binary');
-//
-//   var s3 = new AWS.S3();
-//   s3.client.putObject({
-//     Bucket: 'banners-adxs',
-//     Key: 'del2.txt',
-//     Body: base64data,
-//     ACL: 'public-read'
-//   },function (resp) {
-//     console.log(arguments);
-//     console.log('Successfully uploaded package.');
-//   });
-//
-// });
+function getContentTypeByFile(fileName) {
+  var rc = 'application/octet-stream';
+  var fn = fileName.toLowerCase();
+
+  if (fn.indexOf('.html') >= 0) rc = 'text/html';
+  else if (fn.indexOf('.css') >= 0) rc = 'text/css';
+  else if (fn.indexOf('.json') >= 0) rc = 'application/json';
+  else if (fn.indexOf('.js') >= 0) rc = 'application/x-javascript';
+  else if (fn.indexOf('.png') >= 0) rc = 'image/png';
+  else if (fn.indexOf('.jpg') >= 0) rc = 'image/jpg';
+
+  return rc;
+}
